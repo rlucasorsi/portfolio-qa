8fd13c9bdff9a199e3e2f42b6903a135a2814d20
Lucas Orsi <128734208+rlucasorsi@users.noreply.github.com>
2025-05-20 08:44:02 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={[inter.variable, calSans.variable].join(" ")}>
       <body
-        className={`bg-slate-700 text-slate-100 ${
+        className={`bg-zinc-950 text-zinc-100 ${
           process.env.NODE_ENV === "development" ? "debug-screens" : ""
         }`}
       >
