7de2c85d0fb0d6a2621afb93a3e808cb0dc8c74a
Lucas Orsi <128734208+rlucasorsi@users.noreply.github.com>
2025-05-21 08:59:33 -0300

    Update formation-list.tsx

--- src/components/formation-list.tsx
+++ src/components/formation-list.tsx
@@ -7,14 +7,14 @@ import { CardContent, CardHeader, CardTitle } from './ui/card'
 
 export function FormationList() {
   return (
-    <section className="space-y-4 max-w-2xl mx-auto lg:mx-0">
+    <section className="space-y-4 max-w-2xl mx-auto lg:mx-0 bg-red-500">
       <h2 className="z-10 text-lg sm:text-2xl md:text-3xl text-transparent cursor-default text-edge-outline font-display bg-clip-text bg-gradient-radial-yellow tracking-wider mb-4">
         Formação Acadêmica
       </h2>
       
       <div className="w-full h-px bg-zinc-800 mb-4" />
       
-      <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2">
+      <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 bg-blue-500">
         {userInfo.formation.map((formation) => (
           <Card key={formation.name} className="bg-zinc-900 border border-zinc-800 rounded-lg overflow-hidden">
             <CardHeader className="flex flex-row items-center space-x-4 pb-2">
